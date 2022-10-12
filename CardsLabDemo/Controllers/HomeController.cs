4082be82df86c663aa1ce7802751d0bba21c4cf2
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-10-12 12:04:48 -0400

    working i think

--- CardsLabDemo/Controllers/HomeController.cs
+++ CardsLabDemo/Controllers/HomeController.cs
@@ -21,28 +21,35 @@ namespace CardsLabDemo.Controllers
 
         public async Task<IActionResult> Index()
         {
-            HttpClient web = new HttpClient();
-            web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
-            var connection = await web.GetAsync($"new/shuffle/?deck_count=1");
-            CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
-            //DeckId = resp.deck_id; // Not doing this after all.
+            //HttpClient web = new HttpClient();
+            //web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+            //// First use of our HttpClient below
+            //var connection = await web.GetAsync($"new/shuffle/?deck_count=1");
+            //CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+            ////DeckId = resp.deck_id; // Not doing this after all.
 
-            connection = await web.GetAsync($"{resp.deck_id}/draw/?count=5");
-            resp = await connection.Content.ReadAsAsync<CardResponse>();
+            //// Second use of our HttpClient instance
+            //connection = await web.GetAsync($"{resp.deck_id}/draw/?count=5");
+            //resp = await connection.Content.ReadAsAsync<CardResponse>();
+
+            string deck_id = await CardAPI.GetNewDeck();
+            CardResponse resp = await CardAPI.GetCards(deck_id, 5);
 
             return View(resp);
         }
 
         public async Task<IActionResult> DrawFive(string deck_id)
         {
-            HttpClient web = new HttpClient();
-            web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
-            var connection = await web.GetAsync($"{deck_id}/draw/?count=5");
-            if (connection.StatusCode.ToString() == "NotFound")
-            {
-                return Content(connection.RequestMessage.RequestUri.OriginalString);
-            }
-            CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+            // Old code before refactoring with CardAPI Dal
+            //HttpClient web = new HttpClient();
+            //web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+            //var connection = await web.GetAsync($"{deck_id}/draw/?count=5");
+            //if (connection.StatusCode.ToString() == "NotFound")
+            //{
+            //    return Content(connection.RequestMessage.RequestUri.OriginalString);
+            //}
+            //CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+            CardResponse resp = await CardAPI.GetCards(deck_id, 5);
             return View("index", resp);
         }
 
--- CardsLabDemo/Models/CardResponse.cs
+++ CardsLabDemo/Models/CardResponse.cs
@@ -15,4 +15,71 @@
         public string value { get; set; }
         public string suit { get; set; }
     }
+
+
+    public class CardAPI
+    {
+        // This is the DAL for the API calls
+        // Let's encapsulate the API calls into easy-to-use methods
+        // We'll have two methods
+            // GetNewDeck()
+            // GetCards(deck_id, count)
+
+        // DO NOT access this variable directly.
+        // When you need an HttpClient instance, call GetHttpClient()
+        public static HttpClient _web = null;
+
+        public static HttpClient GetHttpClient()
+        {
+            // We call this concept a "singleton"
+
+            // First check if we already have an HttpClient.
+            // If so, return that one.
+            // Else create one and return that one.
+            //
+            // Or, flip the logic
+            //
+            // First check if we already have an HttpClient.
+            // If not, create one.
+            // Then regardless, return it.
+            
+
+            if (_web == null)
+            {
+                _web = new HttpClient();
+                _web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+
+            }
+
+            return _web;
+        }
+
+        public static async Task<String> GetNewDeck()
+        {
+            HttpClient web = GetHttpClient();
+            // Removed this one below (that broke me earlier at noon)
+            // because we're setting the base address in GetHttpClient()
+            //web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+
+
+            var connection = await web.GetAsync($"new/shuffle/?deck_count=1");
+            CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+
+            return resp.deck_id;
+        }
+
+        public static async Task<CardResponse> GetCards(string deck_id, int count)
+        {
+            HttpClient web = GetHttpClient();
+            var connection = await web.GetAsync($"{deck_id}/draw/?count={count}");
+            CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+
+            return resp;
+        }
+
+
+
+    }
+
+
 }
