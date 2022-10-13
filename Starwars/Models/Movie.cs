93cdb84875d1500af246bef2067eb53f05dfe199
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-10-13 10:30:14 -0400

    added starwars project

---
+++ Starwars/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Starwars.Models
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public int year { get; set; }
+        public List<string> names { get; set; }
+        public List<string> ships { get; set; }
+    }
+}
